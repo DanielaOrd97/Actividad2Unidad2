2c2340bea1d4a0987aac7a1c56dd599b462da5ff
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-10-08 19:08:48 -0600

    ....

---
+++ Act2Unidad2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Act2Unidad2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
