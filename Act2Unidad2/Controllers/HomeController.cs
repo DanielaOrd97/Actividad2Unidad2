ae6093d3cedf221dceb255a41ebd4c0f922dc9ae
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-10-09 23:38:00 -0600

    c

--- Act2Unidad2/Controllers/HomeController.cs
+++ Act2Unidad2/Controllers/HomeController.cs
@@ -1,24 +1,37 @@
 ﻿using Act2Unidad2.Models.Entities;
 using Act2Unidad2.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Act2Unidad2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(char letra)
         {
             PerrosContext context = new();
-
-            var datos = context.Razas.OrderBy(x => x.Nombre).Select(x => new RazasViewModel
-            {
-                IdRaza = (int)x.Id,
-                NombreRaza = x.Nombre
-            });
+           
+                var datos = context.Razas.OrderBy(x => x.Nombre).Select(x => new IndexViewModel
+                {
+                    IdRaza = (int)x.Id,
+                    NombreRaza = x.Nombre
+                });
 
             return View(datos);
         }
 
+        //public IActionResult Filtro(char letra)
+        //{
+        //    PerrosContext context = new();
+
+        //    var datos = context.Razas.OrderBy(x => x.Nombre).Where(x => x.Nombre[0] == letra).Select(x => new IndexViewModel
+        //    {
+        //        IdRaza = (int)x.Id,
+        //        NombreRaza = x.Nombre
+        //    });
+
+        //    return View(datos);
+        //}
 
     }
 }
---
+++ Act2Unidad2/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Act2Unidad2.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public int IdRaza { get; set; }
+        public string NombreRaza { get; set; } = null!;
+        //public IEnumerable<RazasModel> ListaRazas { get; set; } = null!;
+    }
+
+    //public class RazasModel
+    //{
+    //    public int IdRaza { get; set; }
+    //    public string NombreRaza { get; set; } = null!;
+    //}
+
+}
